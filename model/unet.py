06cfb91014699851236a3eef31547eaab5156b35
Niranjan Ajay Ghone <96878836+TheSkyEr4998@users.noreply.github.com>
2026-03-26 17:15:25 +0000

    Refactor comments in build_unet function

--- model/unet.py
+++ model/unet.py
@@ -3,17 +3,17 @@ from tensorflow.keras import layers, Model
 def build_unet(input_shape=(256, 256, 1)):
     inputs = layers.Input(input_shape)
     
-    # Simple Encoder
+    # Encoder
     c1 = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
     p1 = layers.MaxPooling2D((2, 2))(c1)
     
     # Bottleneck
     bn = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(p1)
     
-    # Simple Decoder
+    # Decoder
     u1 = layers.UpSampling2D((2, 2))(bn)
     concat = layers.Concatenate()([u1, c1])
     c2 = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(concat)
     
-    outputs = layers.Conv2D(1, (1, 1), activation='sigmoid')(c2) # [cite: 131]
+    outputs = layers.Conv2D(1, (1, 1), activation='sigmoid')(c2)
     return Model(inputs, outputs)
