5360cada062340e930e6b868ee1ce9c0d09bc71c
Niranjan Ajay Ghone <96878836+TheSkyEr4998@users.noreply.github.com>
2026-03-26 17:11:00 +0000

    Add reptile_update function to update meta-weights
    Implement reptile_update function for meta-learning.

---
+++ model/meta_learner.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def reptile_update(meta_model, task_model, epsilon=0.1):
+    """Updates meta-weights towards task-specific weights[cite: 135]."""
+    new_weights = []
+    meta_weights = meta_model.get_weights()
+    task_weights = task_model.get_weights()
+    
+    for m_w, t_w in zip(meta_weights, task_weights):
+        new_weights.append(m_w + epsilon * (t_w - m_w))
+    
+    meta_model.set_weights(new_weights)
