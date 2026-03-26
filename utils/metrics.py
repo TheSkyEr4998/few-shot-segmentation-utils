fc4dc5fff657c146277741e6b0449a61726974ad
Niranjan Ajay Ghone <96878836+TheSkyEr4998@users.noreply.github.com>
2026-03-26 17:34:36 +0000

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 
 def dice_coef(y_true, y_pred, smooth=1e-6):
-    """Dice Similarity Coefficient implementation."""
     y_true_f = tf.reshape(tf.cast(y_true, tf.float32), [-1])
     y_pred_f = tf.reshape(tf.cast(y_pred > 0.5, tf.float32), [-1])
     intersection = tf.reduce_sum(y_true_f * y_pred_f)
