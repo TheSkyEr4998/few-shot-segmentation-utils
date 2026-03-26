7675f199310fff00c1c9d1b978de6033e8e938f9
Niranjan Ajay Ghone <96878836+TheSkyEr4998@users.noreply.github.com>
2026-03-26 17:09:31 +0000

    Add Dice Similarity Coefficient calculation function

---
+++ utils/metrics.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+def dice_coef(y_true, y_pred, smooth=1e-6):
+    """Calculates the Dice Similarity Coefficient."""
+    y_true_f = tf.reshape(tf.cast(y_true, tf.float32), [-1])
+    y_pred_f = tf.reshape(tf.cast(y_pred > 0.5, tf.float32), [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
